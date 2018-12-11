9ce218be6ef728d526edaa3f29a943469e64abe2
ABCmoxun <41811795+ABCmoxun@users.noreply.github.com>
2018-12-11 21:24:10 +0800

    Update guge_api.py
    更新18-12-11/21：00

--- 爬虫/验证码识别借口/guge_api.py
+++ 爬虫/验证码识别借口/guge_api.py
@@ -1,11 +1,21 @@
 #-*- coding:utf-8 -*-
-#谷歌借口
 
+百度识别文档
+http://ai.baidu.com/docs#/OCR-Python-SDK/07883957
+开发资源---文档中心---视觉技术下的文字识别---->API文档---SDK文档中的python语言
+
+
+#谷歌借口
+安装PIL:pip install Pillow
+安装pytesseract: pip install pytesseract
+安装esseract-ocr: pip insatll tesseract-ocr 
+  
 from PIL import Image
-from pytesseract import *
+import pytesseract
+#img = img.convert('L')灰度化
 # 加载图片
 image = Image.open('test3.jpg')
 # 识别过程
-text = image_to_string(image)
+text = pytesseract.image_to_string(image,lang='chi_sim') #使用简体中文解析图片
 print(text)
 
